fe3dff23fd7219dc589b42c11a4cac5ea3c6deb4
Piyush Kumar <piyushk461@gmail.com>
2025-04-13 01:08:04 +0530

    added dataase

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import { create, router as _router, defaults } from 'json-server';
-const server = create();
-const router = _router("db.json");
-const middlewares = defaults();
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults({
+  static: false
+});
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port, () => {
-  console.log(`JSON Server is running at http://localhost:${port}`);
+  console.log(`JSON Server is running on port ${port}`);
 });
